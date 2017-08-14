a9679451d37115aed3df936f1be49a48060e7aea
Jakub Sikora <jakub.sikora@pure360.com>
2017-08-14 16:16:21 +0100

    new modes

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -2,9 +2,17 @@ const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
 const ZipPlugin = require('zip-webpack-plugin');
+const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir);
+}
 
 module.exports = function () {
   return webpackMerge(baseConfig(), {
+    output: {
+      path: resolve('dist/zip'),
+    },
     plugins: [
       new webpack.DefinePlugin({
         'process.env': {
