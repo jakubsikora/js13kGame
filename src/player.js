c0cc2b3cc51b61a96ae602be74a8ac41b40fad4d
Jakub Sikora <jakub.sikora@pure360.com>
2017-08-21 10:17:43 +0100

    player

--- src/player.js
+++ src/player.js
@@ -6,6 +6,9 @@ import {
   A_CHARACTER,
 } from './constants';
 
+const IMAGE_WIDTH = 50;
+const IMAGE_HEIGHT = 75;
+
 export default class Player {
   constructor(x, y, map) {
     this.ctx = canvas.getContext('2d');
@@ -15,6 +18,35 @@ export default class Player {
     this.y = y;
     this.vel = 0;
     this.map = map;
+    this.direction = 'N';
+    this.image = assets.getByName(A_CHARACTER);
+
+    this.directionMap = {
+      S: {
+        w: IMAGE_WIDTH / 4,
+        h: IMAGE_HEIGHT / 4,
+        x: 0,
+        y: 0,
+      },
+      N: {
+        w: IMAGE_WIDTH / 4,
+        h: IMAGE_HEIGHT / 4,
+        x: 0,
+        y: 0 + (IMAGE_HEIGHT / 4),
+      },
+      W: {
+        w: IMAGE_WIDTH / 4,
+        h: IMAGE_HEIGHT / 4,
+        x: 0,
+        y: 0 + (2 * (IMAGE_HEIGHT / 4)),
+      },
+      E: {
+        w: IMAGE_WIDTH / 4,
+        h: IMAGE_HEIGHT / 4,
+        x: 0,
+        y: 0 + (3 * (IMAGE_HEIGHT / 4)),
+      },
+    };
   }
 
   update() {
@@ -22,21 +54,25 @@ export default class Player {
 
     if (keys.isPressed('ArrowUp')) {
       this.y -= 1;
+      this.direction = 'N';
       pressed = true;
     }
 
     if (keys.isPressed('ArrowDown')) {
       this.y += 1;
+      this.direction = 'S';
       pressed = true;
     }
 
     if (keys.isPressed('ArrowLeft')) {
       this.x -= 1;
+      this.direction = 'W';
       pressed = true;
     }
 
     if (keys.isPressed('ArrowRight')) {
       this.x += 1;
+      this.direction = 'E';
       pressed = true;
     }
 
@@ -52,12 +88,25 @@ export default class Player {
   }
 
   render() {
-    this.ctx.fillStyle = '#FF0000';
-    this.ctx.fillRect(
+    // this.ctx.fillStyle = '#FF0000';
+    // this.ctx.fillRect(
+    //   this.x,
+    //   this.y,
+    //   this.w,
+    //   this.h,
+    // );
+    this.directionCoords = this.directionMap[this.direction];
+
+    this.ctx.drawImage(
+      this.image,
+      this.directionCoords.x,
+      this.directionCoords.y,
+      this.directionCoords.w,
+      this.directionCoords.h,
       this.x,
       this.y,
-      this.w,
-      this.h,
+      IMAGE_WIDTH / 4,
+      IMAGE_HEIGHT / 4,
     );
   }
 }
